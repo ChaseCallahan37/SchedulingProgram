1a2f26d62da9ce829a24b0b7186a057b4a336430
ChaseCallahan37 <97930526+ChaseCallahan37@users.noreply.github.com>
2022-06-16 17:47:20 -0500

    minor tweaks

--- scheduling-app/src/app-info/course-info.js
+++ scheduling-app/src/app-info/course-info.js
@@ -1,41 +1,51 @@
 import Course from "./../Classes/course-class";
 import { createBlankAvailability } from "./../Classes/availability-class";
 
+const MIS221Avail = [
+  {
+    day: "monday",
+    times: {
+      start: 1000,
+      end: 1200,
+    },
+  },
+  {
+    day: "tuesday",
+    times: {
+      start: 1500,
+      end: 1700,
+    },
+  },
+];
+
+const MIS321Avail = [
+  {
+    day: "wednesday",
+    times: {
+      start: 900,
+      end: 1100,
+    },
+  },
+  {
+    day: "tuesday",
+    times: {
+      start: 1130,
+      end: 1545,
+    },
+  },
+];
+
 let courses = [
   new Course({
     title: "MIS 221",
     info: "This course covers coding and such",
-    availability: createBlankAvailability(),
+    availability: createBlankAvailability([...MIS221Avail]),
     resources: ["Insert Resources"],
   }),
   new Course({
     title: "MIS 321",
     info: "Here we talk about more coding",
-    availability: createBlankAvailability(),
-    resources: ["Insert Resources"],
-  }),
-  new Course({
-    title: "MIS 421",
-    info: "Here we talk about advanced coding ",
-    availability: createBlankAvailability(),
-    resources: ["Insert Resources"],
-  }),
-  new Course({
-    title: "MIS 500",
-    info: "This is a course I made up",
-    availability: createBlankAvailability(),
-    resources: ["Insert Resources"],
-  }),
-  new Course({
-    title: "MIS 501",
-    info: "The second course that I made up",
-    availability: createBlankAvailability(),
-    resources: ["Insert Resources"],
-  }),
-  new Course({
-    title: "MIS 502",
-    info: "The most fake course there is",
-    availability: createBlankAvailability(),
+    availability: createBlankAvailability([...MIS321Avail]),
     resources: ["Insert Resources"],
   }),
 ];
